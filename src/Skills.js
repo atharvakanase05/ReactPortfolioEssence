036b936d532527d163e9ae067d089c4541805f97
atharvakanase05 <atharvakanase594@gmail.com>
2025-03-13 15:55:30 +0530

    Initial commit of my React portfolio

---
+++ src/Skills.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Skills.css'; // Create this CSS file as well
+
+function Skills() {
+    return (
+        <div className="content skills">
+            <h2>My Skills</h2>
+            <ul>
+                <li>HTML & CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>Java & Python</li>
+                <li>MongoDB</li>
+                <li>Git & GitHub</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Skills;
